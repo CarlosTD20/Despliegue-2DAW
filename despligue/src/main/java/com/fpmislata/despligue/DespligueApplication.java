63e5592ddc205392fabb26f741afcf95ed64a3c9
CarlosTD20 <carlostornero204@gmail.com>
2023-10-20 13:03:44 +0200

    Start node project and add pom plugin to run node commands when compiling

---
+++ despligue/src/main/java/com/fpmislata/despligue/DespligueApplication.java
@@ -0,0 +1,14 @@
+package com.fpmislata.despligue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespligueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespligueApplication.class, args);
+		System.out.println("Hola Mundo");
+	}
+
+}
---
+++ despligue/src/test/java/com/fpmislata/despligue/DespligueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpmislata.despligue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespligueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
